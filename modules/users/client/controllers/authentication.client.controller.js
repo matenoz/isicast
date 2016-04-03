2f93fda828e4dadc70593bc3599caeb647f0349e
Massimo Esposito <matenino@gmail.com>
2016-04-03 16:14:26 +0200

    signin fixes

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -13,45 +13,45 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
       $location.path('/');
     }
 
-    $scope.signup = function (isValid) {
-      $scope.error = null;
+    // $scope.signup = function (isValid) {
+    //   $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
+    //   if (!isValid) {
+    //     $scope.$broadcast('show-errors-check-validity', 'userForm');
 
-        return false;
-      }
+    //     return false;
+    //   }
 
-      $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
-        // If successful we assign the response to the global user model
-        $scope.authentication.user = response;
+    //   $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
+    //     // If successful we assign the response to the global user model
+    //     $scope.authentication.user = response;
 
-        // And redirect to the previous or home page
-        $state.go($state.previous.state.name || 'home', $state.previous.params);
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
+    //     // And redirect to the previous or home page
+    //     $state.go($state.previous.state.name || 'home', $state.previous.params);
+    //   }).error(function (response) {
+    //     $scope.error = response.message;
+    //   });
+    // };
 
-    $scope.signin = function (isValid) {
-      $scope.error = null;
+    // $scope.signin = function (isValid) {
+    //   $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
+    //   if (!isValid) {
+    //     $scope.$broadcast('show-errors-check-validity', 'userForm');
 
-        return false;
-      }
+    //     return false;
+    //   }
 
-      $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
-        // If successful we assign the response to the global user model
-        $scope.authentication.user = response;
+    //   $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
+    //     // If successful we assign the response to the global user model
+    //     $scope.authentication.user = response;
 
-        // And redirect to the previous or home page
-        $state.go($state.previous.state.name || 'home', $state.previous.params);
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
+    //     // And redirect to the previous or home page
+    //     $state.go($state.previous.state.name || 'home', $state.previous.params);
+    //   }).error(function (response) {
+    //     $scope.error = response.message;
+    //   });
+    // };
 
     // OAuth provider request
     $scope.callOauthProvider = function (url) {
